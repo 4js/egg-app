7bbe5f3813ef25fc43eaa0d18b045b429f78c33f
fengxianqin <fengxianqin01@chinatopcredit.com>
2019-12-01 00:23:11 +0800

    产品和学习路线创建表和接口

--- app/controller/product.js
+++ app/controller/product.js
@@ -3,6 +3,11 @@
 const Controller = require('egg').Controller;
 
 class ProductController extends Controller {
+  /**
+   * 获取所有产品列表
+   * @method {get}
+   */
+
   async list() {
     const { ctx } = this;
     const result = await ctx.service.product.list();
--- app/controller/tail.js
+++ app/controller/tail.js
@@ -3,6 +3,11 @@
 const Controller = require('egg').Controller;
 
 class TailController extends Controller {
+  /**
+   * 获取所有学习路线列表
+   * @method {get}
+   */
+
   async list() {
     const { ctx } = this;
     const result = await ctx.service.tail.list();
